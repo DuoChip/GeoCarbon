c97106f27e84a0c90170c12e12983c6cb2b42585
DuoChip <duoc7090@gmail.com>
2025-06-19 20:40:16 +0700

    Update 8

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ $("#contactForm").submit(function (e) {
     alert("Cảm ơn bạn đã liên hệ! Chúng tôi sẽ phản hồi sớm nhất.");
     this.reset();
 });
+
+
+
+
+/* ****** TEAM ****** */
+
+ // Smooth scrolling
+$('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    const target = $(this.getAttribute('href'));
+    if (target.length) {
+        $('html, body').animate({
+            scrollTop: target.offset().top - 80
+        }, 800);
+    }
+});
+        
+// Add shadow to navbar on scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar').addClass('shadow-sm');
+    } else {
+        $('.navbar').removeClass('shadow-sm');
+    }
+});
